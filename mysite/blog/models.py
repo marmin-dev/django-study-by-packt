4fba14f7ecea6c846d1e1424832fa8cae499abb8
박태욱 <marmin.dev@gmail.com>
2025-06-12 10:45:50 +0900

    Chore: 모델 정의

---
+++ mysite/blog/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF','Draft'
+        PUBLISHED = 'PB','Published'
+        
+    title = models.CharField(max_length=250)
+    # Varchar 로 변환되는 슬러그 필드
+    # 슬러그는 문자 숫자 밑줄 또는 하이픈만 포함
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT,
+    )
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+    def __str__(self):
+        return self.title
