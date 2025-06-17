fcf4c5013ebcb141127885bb59779a59634cd97b
박태욱 <marmin.dev@gmail.com>
2025-06-17 15:37:16 +0900

    Feat: views 및 템플릿 추가

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+import blog
+from .models import Post
+
+
+# 포스트 목록
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts':posts})
+
+
+# 포스트 상세 페이지
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post':post})
