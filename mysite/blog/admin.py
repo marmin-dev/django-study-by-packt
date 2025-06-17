96d1714f23ac828da5988c5140fe9b18f5ed9b2d
박태욱 <marmin.dev@gmail.com>
2025-06-17 09:48:01 +0900

    Feat: Admin, 모델 개선

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from blog.models import Post
 
-# Register your models here.
+
+# admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'created'] # 리스트에 보여질 목록
+    list_filter = ['author', 'publish', 'created', 'author'] # 속성에 포함된 필드별로 결과 필터링
+    search_fields = ['title', 'body'] # 검색 조건
+    prepopulated_fields = {'slug': ('title',)} # 타
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ('status','publish')
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
@@ -7,11 +8,13 @@ class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF','Draft'
         PUBLISHED = 'PB','Published'
-        
+
     title = models.CharField(max_length=250)
     # Varchar 로 변환되는 슬러그 필드
     # 슬러그는 문자 숫자 밑줄 또는 하이픈만 포함
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
