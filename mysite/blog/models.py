217dd33890d98e2c64bec5402ae147f9f9cd3842
박태욱 <marmin.dev@gmail.com>
2025-06-17 10:05:46 +0900

    Feat: 모델 매니저 추가

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,11 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (super(PublishedManager, self).get_queryset()
+                .filter(status=Post.Status.PUBLISHED))
+
 
 class Post(models.Model):
 
@@ -24,6 +29,9 @@ class Post(models.Model):
         choices=Status.choices,
         default=Status.DRAFT,
     )
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
