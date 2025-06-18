693de18d15d5aa30e996902481d2c16f2f66ccc4
박태욱 <marmin.dev@gmail.com>
2025-06-18 11:01:11 +0900

    Feat: 페이지네이션 추가

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -17,7 +18,9 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     # Varchar 로 변환되는 슬러그 필드
     # 슬러그는 문자 숫자 밑줄 또는 하이픈만 포함
-    slug = models.SlugField(max_length=250)
+    # 지정된 날짜의 중복을 허용하지 않게 된다.
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='blog_posts')
     body = models.TextField()
@@ -41,3 +44,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    # url 불러오기
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,7 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
+
 
 import blog
 from .models import Post
@@ -7,15 +9,30 @@ from .models import Post
 
 # 포스트 목록
 def post_list(request):
-    posts = Post.objects.all()
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        # page_number 이 정수가 아닌 경우 첫 번째 페이지 전달
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
                   {'posts':posts})
 
 
 # 포스트 상세 페이지
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    # SEO 개선을 위해 연, 월, 일 사용
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request,
                   'blog/post/detail.html',
                   {'post':post})
