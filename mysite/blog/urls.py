161978394a1f955f0b9be4d2b4ad712a2df520cc
박태욱 <marmin.dev@gmail.com>
2025-06-18 09:49:47 +0900

    Feat: Blog 페이지 추가, 1챕터 끝

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,6 +5,6 @@ from blog import views
 app_name = 'blog'
 urlpatterns = [
     # post 뷰
-    path('',views.post_list,name='post_list'),
-    path('<int:id>/',views.post_detail,name='post_detail'),
+    path('', views.post_list, name='post_list'),
+    path('<int:id>/', views.post_detail, name='post_detail'),
 ]
