384b8ff8b39f6057fe05054ada83f5c340d76fc5
박태욱 <marmin.dev@gmail.com>
2025-06-26 09:13:17 +0900

    Feat: Markdown 추가

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,6 +1,10 @@
 from django import template
+from django.utils.safestring import mark_safe
+
 from blog.models import Post
 from django.db.models import Count
+
+import markdown
 
 register = template.Library()
 
@@ -11,6 +15,7 @@ def total_posts():
 
 # 최근 게시물 5개 보여주는 커스텀 태그
 @register.inclusion_tag('blog/post/latest_posts.html')
+# 인클루전 태그를 사용하면 템플릿 태그에서 반환된 콘텍스트 변수로 템플릿을 렌더링 할 수 있다.
 def show_latest_posts(count=5):
     latest_posts = Post.objects.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
@@ -21,3 +26,9 @@ def get_most_commented_posts(count=5):
     return Post.published.annotate(
         total_comments = Count('comments')
     ).order_by('-total_comments')[:count]
+
+
+# 게시물에서 마크다운 구문 사용 게시물 본문 HTML 로 변환 가능 커스텀 필터
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
