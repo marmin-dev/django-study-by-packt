f86bf488a4144965b125608ec1f081a465943f40
박태욱 <marmin.dev@gmail.com>
2025-06-26 09:00:26 +0900

    Feat: 댓글 많은 순 리스트 커스텀 태그 추가

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,13 +1,23 @@
 from django import template
 from blog.models import Post
+from django.db.models import Count
 
 register = template.Library()
 
+# 게시물 개수 보여주는 커스텀 태그
 @register.simple_tag
 def total_posts():
     return Post.objects.count()
 
+# 최근 게시물 5개 보여주는 커스텀 태그
 @register.inclusion_tag('blog/post/latest_posts.html')
 def show_latest_posts(count=5):
     latest_posts = Post.objects.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+# 댓글 많은 순 리스트 보여주는 커스텀 태그
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(
+        total_comments = Count('comments')
+    ).order_by('-total_comments')[:count]
