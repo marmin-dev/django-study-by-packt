37f1aa4929c68fc8463978cd6748fb5ff3e11703
박태욱 <marmin.dev@gmail.com>
2025-06-26 09:47:50 +0900

    Chore: Requirements.txt

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -8,7 +8,10 @@ sitemaps = {
     'posts' : PostSitemap
 }
 urlpatterns = [
+    # 관리자
     path('admin/', admin.site.urls),
+    # 블로그 애플리케이션
     path('blog/',include('blog.urls', namespace='blog')),
+    # 사이트 맵
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
