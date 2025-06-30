1a5e946bc55d3a2180466d2e270ad5dec55723e9
박태욱 <marmin.dev@gmail.com>
2025-06-30 15:40:43 +0900

    Feat: 회원가입 추가

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,6 +1,23 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     # HTML 에 password 입력으로 취급하도록
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label="Password",widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Repeat Password",widget=forms.PasswordInput)
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'first_name', 'email']
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("Passwords don't match")
+        return cd['password2']
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 
-from account.forms import LoginForm
+from account.forms import LoginForm, UserRegistrationForm
 
 
 # 로그인
@@ -27,6 +27,27 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html',{'form':form})
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # 유저 객체를 생성하지만 실제 저장은 하지 않음
+            new_user = user_form.save(commit=False)
+            # 새로운 패스워드 선택
+            new_user.set_password(user_form.cleaned_data['password'])
+            # 오브젝트 저장
+            new_user.save()
+            return render(request, 'account/register_done.html',{'new_user':new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(
+        request,
+        'account/register.html',
+        {'user_form':user_form}
+    )
+
 
 
 @login_required
