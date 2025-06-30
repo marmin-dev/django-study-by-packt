9136eab914299f680dd5b2988c34ac9268cccf26
박태욱 <marmin.dev@gmail.com>
2025-06-30 14:24:41 +0900

    Feat: 소셜앱 비밀번호 변경 기능추가

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -6,8 +6,20 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     # 로그인
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    # 로그아웃
+    # 로그아웃  - POST 메서드만 동작한다.
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     # 대시보드
     path('', views.dashboard, name='dashboard'),
+    # 비밀 번호 변경
+    path(
+        'password-change/',
+        auth_views.PasswordChangeView.as_view(),
+        name='password_change'
+    ),
+    # 비밀 번호 변경 완료
+    path(
+        'password-change/done/',
+        auth_views.PasswordChangeDoneView.as_view(),
+        name='password_change_done'
+    )
 ]
