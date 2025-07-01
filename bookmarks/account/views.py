2a2561949f7a1077bc3f8db6def383db1fb4da1d
박태욱 <marmin.dev@gmail.com>
2025-07-01 09:37:48 +0900

    Feat: Adding Messages on edit profile

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 
 from account.models import Profile
 from account.forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+
 
 
 # 로그인
@@ -72,6 +73,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, "Profile updated successfully")
+        else:
+            messages.error(request, "Error updating your profile")
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
@@ -83,3 +87,5 @@ def edit(request):
             "profile_form":profile_form
         }
     )
+
+
